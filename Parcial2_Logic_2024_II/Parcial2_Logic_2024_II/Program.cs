5dfcb922cd1f8daf5e5ba2fb889aaa4a4260d34f
Alejandro Berrio <alejo2626@hotmail.com>
2024-10-07 20:57:48 -0500

    metodo case

--- Parcial2_Logic_2024_II/Parcial2_Logic_2024_II/Program.cs
+++ Parcial2_Logic_2024_II/Parcial2_Logic_2024_II/Program.cs
@@ -31,10 +31,60 @@ internal class Program
             int Turno = 0;
 
             Console.Clear();
-            Console.WriteLine($"¡Bienvenidos al juego! El número ha sido seleccionado entre 0 y {ObtenerRango(cantidadJugadores)}.");
+            Console.WriteLine($"¡Bienvenidos al juego! El número ha sido seleccionado entre 0 y 100 {ObtenerRango(cantidadJugadores)}.");
 
             while (!ganador)
 
+            {
+                Console.WriteLine($"\nTurno del jugador {turno + 1}:");
+                Console.Write("Ingrese su número: ");
+                int intento = int.Parse(Console.ReadLine());
+
+                if (intento > numeroAleatorio)
+                {
+                    Console.WriteLine("MENOR");
+                }
+                else if (intento < numeroAleatorio)
+                {
+                    Console.WriteLine("MAYOR");
+                }
+                else
+                {
+                    Console.WriteLine("¡HAS GANADO!");
+                    ganador = true;
+                }
+
+                // Cambiar de turno del juego
+                Turno = (Turno + 1) % cantidadJugadores;
+            }
+
+            // Preguntar si quieren jugar de nuevo
+
+            Console.WriteLine("\n¿Desean jugar de nuevo? (s/n)");
+            char respuesta = Console.ReadKey().KeyChar;
+            jugar = (respuesta == 's' || respuesta == 'S');
+            Console.Clear();
+        }
+    }
+
+    static int GenerarNumeroAleatorio(int cantidadJugadores, Random random)
+    {
+        int rangoMaximo = ObtenerRango(cantidadJugadores);
+        return random.Next(0, rangoMaximo + 1);
+    }
+
+    static int ObtenerRango(int cantidadJugadores)
+    {
+        switch (cantidadJugadores)
+        {
+            case 2:
+                return 50;
+            case 3:
+                return 100;
+            case 4:
+                return 200;
+            default:
+                return 0; // No debería llegar aquí
 
 
 

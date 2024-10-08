4e00bf6cc41df331bf69c325d13c30e4d0934bbf
Alejandro Berrio <alejo2626@hotmail.com>
2024-10-07 22:15:11 -0500

    pasos

---
+++ Parcial2_Logic_2024_II-punto2/Parcial2_Logic_2024_II-punto2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        //Sistema de evaluación de notas de estudiantes:
+        //El programa permite ingresar las notas de varios estudiantes, calcular su promedio
+        //y luego clasificar a cada estudiante en diferentes categorías de desempeño académico.
+        //Al final, se le pregunta al usuario si quiere ingresar otro grupo de estudiantes.
+        {
+            while (true)
+            {
+                List<double> promedios = new List<double>();
+                List<string> nombres = new List<string>();
+
+    
+    
+}

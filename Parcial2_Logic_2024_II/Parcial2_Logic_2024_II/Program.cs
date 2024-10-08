143b2e64732e48a438dd0c78ebd446a59b075796
Alejandro Berrio <alejo2626@hotmail.com>
2024-10-07 20:33:16 -0500

    entrada

--- Parcial2_Logic_2024_II/Parcial2_Logic_2024_II/Program.cs
+++ Parcial2_Logic_2024_II/Parcial2_Logic_2024_II/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        //Desarrollar el juego “Adivina el número” en un programa de C#. El juego es muy sencillo,
+        //consiste en adivinar un número ℕ aleatorio que el sistema escoge de un determinado rango
+        //y lo guarda en memoria sin ser revelado.
+
+        // valores de entrada
+        random random = new Random();
+        bool jugar = true;
+
+
+        Console.WriteLine("Hello, World!");
+    }
+}

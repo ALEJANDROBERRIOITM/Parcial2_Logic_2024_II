2741932cf74a90f91f25d38a191662ae4be41c32
Alejandro Berrio <alejo2626@hotmail.com>
2024-11-29 20:40:14 -0500

    movimeintos

--- Parcial_Final/Parcial_Final/Program.cs
+++ Parcial_Final/Parcial_Final/Program.cs
@@ -11,7 +11,7 @@
         // Crear y llenar la matriz con números aleatorios
         int[,] matriz = CrearMatriz(filas, columnas);
 
-        // Definir posición inicial y número base
+        // Definir posición inicial y número base principal
         int x = 0, y = 0;
         matriz[x, y] = 0;
 
@@ -22,7 +22,31 @@
             Console.Clear();
             MostrarMatriz(matriz, x, y);
 
-            
+            // Leer entrada del usuario
+            ConsoleKey tecla = Console.ReadKey().Key;
+
+            // Manejar movimiento o salida
+            switch (tecla)
+            {
+                case ConsoleKey.UpArrow:
+                    if (x > 0) x--;
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (x < filas - 1) x++;
+                    break;
+                case ConsoleKey.LeftArrow:
+                    if (y > 0) y--;
+                    break;
+                case ConsoleKey.RightArrow:
+                    if (y < columnas - 1) y++;
+                    break;
+                case ConsoleKey.Escape:
+                    continuar = false; // Salir del programa
+                    break;
+            }
+        }
+
+        
 
     }
 }

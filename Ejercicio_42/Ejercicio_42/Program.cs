a91b7521aa051a24e0af569d59257cb85f663a6a
Alejandro Berrio <alejo2626@hotmail.com>
2024-11-29 20:19:59 -0500

    arranque

---
+++ Ejercicio_42/Ejercicio_42/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Desarrolle un algoritmo donde almacene las ventas que obtuvo mensualmente un almacén durante
+        // el año 2023.Mostrar por pantalla cada mes con su respectiva venta en pesos y la suma total
+        // de ventas de dicho año.
+
+
+        //Declaramos los nuevos arrays
+
+        string[] months = { "enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "dieciempre" };
+        decimal[] sales = new decimal[12];
+        
+        decimal sum = 0;
+
+        //vamos implementar metodos para trabajar un poco el POO
+        // PRINCIPIO  SOLID
+        //medtodo para lmacenar las ventas
+        //metodo para mostrar ventas
+        // metodo calcular ventas total del año
+
+
+    }
+}
---
+++ Parcial_Final/Parcial_Final/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Solicitar las dimensiones de la matriz
+        Console.Write("Ingrese el número de filas (N): ");
+        int filas = int.Parse(Console.ReadLine());
+        Console.Write("Ingrese el número de columnas (M): ");
+        int columnas = int.Parse(Console.ReadLine());
+    }
+}

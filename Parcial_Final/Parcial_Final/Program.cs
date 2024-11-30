23eb75542c4cf146471fa645cb984e6abe540663
Alejandro Berrio <alejo2626@hotmail.com>
2024-11-29 20:32:08 -0500

    bucle

--- Parcial_Final/Parcial_Final/Program.cs
+++ Parcial_Final/Parcial_Final/Program.cs
@@ -2,10 +2,27 @@
 {
     private static void Main(string[] args)
     {
-        // Solicitar las dimensiones de la matriz
+        // Solicitar dimensiones de la matriz
         Console.Write("Ingrese el número de filas (N): ");
         int filas = int.Parse(Console.ReadLine());
         Console.Write("Ingrese el número de columnas (M): ");
         int columnas = int.Parse(Console.ReadLine());
+
+        // Crear y llenar la matriz con números aleatorios
+        int[,] matriz = CrearMatriz(filas, columnas);
+
+        // Definir posición inicial y número base
+        int x = 0, y = 0;
+        matriz[x, y] = 0;
+
+        // Bucle principal del juego
+        bool continuar = true;
+        while (continuar)
+        {
+            Console.Clear();
+            MostrarMatriz(matriz, x, y);
+
+            
+
     }
 }
